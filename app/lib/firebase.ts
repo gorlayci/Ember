f45d0f286de859db602a620f1a09b35107483eb8
gorlayci <gorlayci@gmail.com>
2026-06-27 22:15:43 -0500

    Update firebase.ts

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,8 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "…",
-  authDomain: "…",
-  projectId: "…",
-  storageBucket: "…",
-  messagingSenderId: "…",
-  appId: "…"
+  apiKey: "AIzaSyCnF5yj51CDmo81JvnFr9ZYojHWnrfyKpo",
+  authDomain: "ember-study-hub.firebaseapp.com",
+  projectId: "ember-study-hub",
+  storageBucket: "ember-study-hub.firebasestorage.app",
+  messagingSenderId: "634838171408",
+  appId: "1:634838171408:web:a3ea75d9558e5d8e39ef95",
+  measurementId: "G-CM38WJFLRH"
 };
